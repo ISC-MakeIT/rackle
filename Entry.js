e3e7d35975a8446ec3979b99e61474cf15b86cd4
yamato0331 <yamatotunasima1@gmail.com>
2019-01-14 22:49:22 +0900

    ダミーデータ削除

--- Entry.js
+++ Entry.js
@@ -1,9 +1,8 @@
 import { KeepAwake, registerRootComponent } from 'expo';
 import App from './src/App';
-import Test from './src/components/Test';
 
 if (__DEV__) {
   KeepAwake.activate();
 }
 
-registerRootComponent(Test);
+registerRootComponent(App);
