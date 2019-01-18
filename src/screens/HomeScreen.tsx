dac94a1041972ba3513d950bca081149f00b080b
fumihumi <t.fum1hum@gmail.com>
2019-01-18 18:52:43 +0900

    lint

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -33,79 +33,79 @@ export default class HomeScreen extends React.Component<Props, State> {
 
     const { height } = Dimensions.get('window');
 
-      return (
-        <View style={styles.container}>
-          <LinearGradient
-            colors={['rgba(99, 191, 142, 0.2)', 'rgba(248, 247, 244, 0.2)', 'white']}
-            style={{
-              position: 'absolute',
-              left: 0,
-              right: 0,
-              top: 0,
-              height: height,
-            }}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 0, y: 1 }}
-          />
+    return (
+      <View style={styles.container}>
+        <LinearGradient
+          colors={['rgba(99, 191, 142, 0.2)', 'rgba(248, 247, 244, 0.2)', 'white']}
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            height: height,
+          }}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 1 }}
+        />
 
-          <View style={styles.appTitleContainer}>
-            <Text style={styles.appName}>横浜駅 </Text>
-            <Text style={styles.appNameKana}>Yokohama</Text>
-          </View>
-
-          <View style={styles.searchFormContainer}>
-            <View style={containerStyles.inputContainer}>
-              <View style={containerStyles.selectContainer} >
-                {/* TODO <View style={styles.fromSelectIcon} /> */}
-
-                <RNPickerSelect
-                  placeholder={{ label: '駅を選択してください', value: null, color: '#9EA0A4', }}
-                  items={options}
-                  onValueChange={(value: number) => { this.setState({ selectedFromValue: value }); }}
-                  style={{...inputPickerStyle}}
-                  value={this.state.selectedFromValue}
-                  useNativeAndroidPickerStyle={false}
-                />
-
-              </View>
-
-              <View style={containerStyles.searchedListContainer}>
-                <Text style={[styles.searchedList, styles.active]} onPress={() => alert('hoge')}>中央北</Text>
-                <Text style={[styles.searchedList]} onPress={() => alert('fuga')}>中央南</Text>
-                <Text style={[styles.searchedList]} onPress={() => alert('foo')}>中央東</Text>
-                <Text style={[styles.searchedList]} onPress={() => alert('var')}>中央西</Text>
-              </View>
-
-            </View>
-            <View style={[styles.toInputContainer, containerStyles.inputContainer]}>
-              <View style={containerStyles.selectContainer}>
-                {/* TODO <View style={styles.fromSelectIcon} /> */}
-                <RNPickerSelect
-                  placeholder={{ label: '駅を選択してください', value: null, color: '#9EA0A4', }}
-                  items={options}
-                  onValueChange={(value: number) => { this.setState({ selectedToValue: value }); }}
-                  style={{...inputPickerStyle}}
-                  value={this.state.selectedToValue}
-                  useNativeAndroidPickerStyle={false}
-                />
-              </View>
-              <View style={containerStyles.searchedListContainer}>
-                <Text style={[styles.searchedList, styles.active]} onPress={() => alert('hoge')}>中央北</Text>
-                <Text style={[styles.searchedList]} onPress={() => alert('fuga')}>中央南</Text>
-                <Text style={[styles.searchedList]} onPress={() => alert('foo')}>中央東</Text>
-                <Text style={[styles.searchedList]} onPress={() => alert('var')}>中央西</Text>
-              </View>
-            </View>
-          </View>
-
-          <TouchableOpacity
-            style={styles.execBtn}
-            onPress={() => this.props.navigation.navigate('Guide')}
-          >
-            <Text style={styles.execText}>案内開始</Text>
-          </TouchableOpacity>
+        <View style={styles.appTitleContainer}>
+          <Text style={styles.appName}>横浜駅 </Text>
+          <Text style={styles.appNameKana}>Yokohama</Text>
         </View>
-      );
+
+        <View style={styles.searchFormContainer}>
+          <View style={containerStyles.inputContainer}>
+            <View style={containerStyles.selectContainer} >
+              {/* TODO <View style={styles.fromSelectIcon} /> */}
+
+              <RNPickerSelect
+                placeholder={{ label: '駅を選択してください', value: null, color: '#9EA0A4', }}
+                items={options}
+                onValueChange={(value: number) => { this.setState({ selectedFromValue: value }); }}
+                style={{...inputPickerStyle}}
+                value={this.state.selectedFromValue}
+                useNativeAndroidPickerStyle={false}
+              />
+
+            </View>
+
+            <View style={containerStyles.searchedListContainer}>
+              <Text style={[styles.searchedList, styles.active]} onPress={() => alert('hoge')}>中央北</Text>
+              <Text style={[styles.searchedList]} onPress={() => alert('fuga')}>中央南</Text>
+              <Text style={[styles.searchedList]} onPress={() => alert('foo')}>中央東</Text>
+              <Text style={[styles.searchedList]} onPress={() => alert('var')}>中央西</Text>
+            </View>
+
+          </View>
+          <View style={[styles.toInputContainer, containerStyles.inputContainer]}>
+            <View style={containerStyles.selectContainer}>
+              {/* TODO <View style={styles.fromSelectIcon} /> */}
+              <RNPickerSelect
+                placeholder={{ label: '駅を選択してください', value: null, color: '#9EA0A4', }}
+                items={options}
+                onValueChange={(value: number) => { this.setState({ selectedToValue: value }); }}
+                style={{...inputPickerStyle}}
+                value={this.state.selectedToValue}
+                useNativeAndroidPickerStyle={false}
+              />
+            </View>
+            <View style={containerStyles.searchedListContainer}>
+              <Text style={[styles.searchedList, styles.active]} onPress={() => alert('hoge')}>中央北</Text>
+              <Text style={[styles.searchedList]} onPress={() => alert('fuga')}>中央南</Text>
+              <Text style={[styles.searchedList]} onPress={() => alert('foo')}>中央東</Text>
+              <Text style={[styles.searchedList]} onPress={() => alert('var')}>中央西</Text>
+            </View>
+          </View>
+        </View>
+
+        <TouchableOpacity
+          style={styles.execBtn}
+          onPress={() => this.props.navigation.navigate('Guide')}
+        >
+          <Text style={styles.execText}>案内開始</Text>
+        </TouchableOpacity>
+      </View>
+    );
   }
 
   public _maybeRenderDevelopmentModeWarning () {
