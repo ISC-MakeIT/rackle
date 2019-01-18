e22bfc8453cb9ae0aeb75b1b238165f8ad09afbf
fumihumi <t.fum1hum@gmail.com>
2019-01-18 17:50:23 +0900

    HomeScreenをそれっぽく実装

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,57 +1,113 @@
 import * as React from 'react';
-import { Platform, ScrollView, StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-elements';
-import { MonoText } from '../components/StyledText';
-import homeStyle from '../stylesheets/HomeScreen.scss';
+import {Platform, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {Button} from 'react-native-elements';
 
-interface Props {
-  navigation: any;
+import {MonoText} from '../components/StyledText';
+import styles from '../stylesheets/HomeScreen.scss';
+import RNPickerSelect from 'react-native-picker-select';
+
+
+interface Props { navigation: any; }
+interface State {
+  selectedFromValue: number;
+  selectedToValue: number;
 }
 
-export default class HomeScreen extends React.Component<Props, {}> {
+export default class HomeScreen extends React.Component<Props, State> {
   public static navigationOptions = {
     title: 'fremap',
   };
 
+  inputRefs: any;
+
   constructor(props: Props) {
     super(props);
+    this.state = { selectedFromValue: 0, selectedToValue: 0, };
   }
 
   public render() {
-    return (
-      <View style={homeStyle.rootContainer}>
-        <ScrollView style={homeStyle.container} contentContainerStyle={homeStyle.contentContainer}>
-          <View style={homeStyle.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-            <Text style={homeStyle.getStartedText}>Get started by opening</Text>
-            <View style={[homeStyle.codeHighlightContainer, homeStyle.homeScreenFilename]}>
-              <MonoText style={homeStyle.codeHighlightText}>screens/HomeScreen.js</MonoText>
+    const options = [
+      { value: 1, label: 'JR' },
+      { value: 2, label: '東横' },
+      { value: 3, label: '相鉄' },
+      { value: 4, label: 'blueline' },
+    ];
+
+      return (
+        <View style={styles.container}>
+
+          <View style={styles.appTitleContainer}>
+            <Text style={styles.appName}>横浜駅 </Text>
+            <Text style={styles.appNameKana}>Yokohama</Text>
+          </View>
+
+          <View style={styles.searchFormContainer}>
+            <View style={containerStyles.inputContainer}>
+              <View style={containerStyles.selectContainer} >
+                {/* TODO <View style={styles.fromSelectIcon} /> */}
+
+                <RNPickerSelect
+                  placeholder={{ label: '駅を選択してください', value: null, color: '#9EA0A4', }}
+                  items={options}
+                  onValueChange={(value: number) => { this.setState({ selectedFromValue: value }); }}
+                  style={{...inputPickerStyle}}
+                  value={this.state.selectedFromValue}
+                  useNativeAndroidPickerStyle={false}
+                />
+
+              </View>
+
+              <View style={containerStyles.searchedListContainer}>
+                <Text style={[styles.searchedList, styles.active]} onPress={() => alert('hoge')}>中央北</Text>
+                <Text style={[styles.searchedList]} onPress={() => alert('fuga')}>中央南</Text>
+                <Text style={[styles.searchedList]} onPress={() => alert('foo')}>中央東</Text>
+                <Text style={[styles.searchedList]} onPress={() => alert('var')}>中央西</Text>
+              </View>
+
             </View>
-            <Text style={homeStyle.getStartedText}>Change this text and your app will automatically reload.</Text>
-            <Button title='change screen' onPress={() => this.props.navigation.navigate('Guide')} />
+            <View style={[styles.toInputContainer, containerStyles.inputContainer]}>
+              <View style={containerStyles.selectContainer}>
+                {/* TODO <View style={styles.fromSelectIcon} /> */}
+                <RNPickerSelect
+                  placeholder={{ label: '駅を選択してください', value: null, color: '#9EA0A4', }}
+                  items={options}
+                  onValueChange={(value: number) => { this.setState({ selectedToValue: value }); }}
+                  style={{...inputPickerStyle}}
+                  value={this.state.selectedToValue}
+                  useNativeAndroidPickerStyle={false}
+                />
+              </View>
+              <View style={containerStyles.searchedListContainer}>
+                <Text style={[styles.searchedList, styles.active]} onPress={() => alert('hoge')}>中央北</Text>
+                <Text style={[styles.searchedList]} onPress={() => alert('fuga')}>中央南</Text>
+                <Text style={[styles.searchedList]} onPress={() => alert('foo')}>中央東</Text>
+                <Text style={[styles.searchedList]} onPress={() => alert('var')}>中央西</Text>
+              </View>
+            </View>
           </View>
-        </ScrollView>
-        <View style={[styles.tabBarInfoContainer, homeStyle.tabBarInfoContainer]}>
-          <Text style={homeStyle.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-          <View style={[homeStyle.codeHighlightContainer, homeStyle.navigationFilename]}>
-            <MonoText style={homeStyle.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
+
+          <Button
+            title='案内開始'
+            style={styles.execBtn}
+            onPress={() => this.props.navigation.navigate('Guide')}
+          />
         </View>
-      </View>
-    );
+      );
   }
 
-  public _maybeRenderDevelopmentModeWarning() {
+  public _maybeRenderDevelopmentModeWarning () {
     if (__DEV__) {
       const learnMoreButton = (<Text> Learn more </Text>);
+
       return (
-        <Text style={homeStyle.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development tools. {learnMoreButton}
+        <Text style={styles.developmentModeText}>
+          Development mode is enabled, your app will be slower but you can use useful
+          development tools. {learnMoreButton}
         </Text>
       );
     } else {
       return (
-        <Text style={homeStyle.developmentModeText}>
+        <Text style={styles.developmentModeText}>
           You are not in development mode, your app will run at full speed.
         </Text>
       );
@@ -59,20 +115,46 @@ export default class HomeScreen extends React.Component<Props, {}> {
   }
 }
 
-const styles = StyleSheet.create({
-  tabBarInfoContainer: {
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: {
-          height: -3,
-        },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
+const containerStyles = StyleSheet.create({
+  inputContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  selectContainer: {
+    marginBottom: 10,
+  },
+  searchedListContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 30,
+  },
+});
+
+const inputPickerStyle = StyleSheet.create({
+  // https://snack.expo.io/HyOOnZymN
+  inputIOS: {
+    fontSize: 16,
+    paddingTop: 13,
+    paddingHorizontal: 10,
+    paddingBottom: 12,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 50,
+    backgroundColor: 'white',
+    color: 'black',
+    minWidth: 300,
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingTop: 13,
+    paddingHorizontal: 10,
+    paddingBottom: 12,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 50,
+    backgroundColor: 'white',
+    color: 'black',
   },
 });
