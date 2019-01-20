ca103972d2c56cfec76e8d0dac649d5a6cee465d
yamato0331 <yamatotunasima1@gmail.com>
2019-01-20 14:44:26 +0900

    カスタムマップを使用

---
+++ src/components/mapComponents/CustomMap.ts
@@ -0,0 +1,93 @@
+export default {
+  mapStyle: [
+    {
+      'featureType': 'administrative.neighborhood',
+      'stylers': [
+        {
+          'visibility': 'off',
+        },
+      ],
+    },
+    {
+      'featureType': 'landscape.man_made',
+      'elementType': 'labels',
+      'stylers': [
+        {
+          'visibility': 'off',
+        },
+      ],
+    },
+    {
+      'featureType': 'landscape.natural.landcover',
+      'elementType': 'labels.text',
+      'stylers': [
+        {
+          'visibility': 'off',
+        },
+      ],
+    },
+    {
+      'featureType': 'poi.business',
+      'stylers': [
+        {
+          'visibility': 'off',
+        },
+      ],
+    },
+    {
+      'featureType': 'poi.park',
+      'elementType': 'labels.text',
+      'stylers': [
+        {
+          'visibility': 'off',
+        },
+      ],
+    },
+    {
+      'featureType': 'road',
+      'elementType': 'labels',
+      'stylers': [
+        {
+          'visibility': 'off',
+        },
+      ],
+    },
+    {
+      'featureType': 'road.arterial',
+      'elementType': 'labels',
+      'stylers': [
+        {
+          'visibility': 'off',
+        },
+      ],
+    },
+    {
+      'featureType': 'road.highway',
+      'elementType': 'labels',
+      'stylers': [
+        {
+          'visibility': 'off',
+        },
+      ],
+    },
+    {
+      'featureType': 'road.local',
+      'stylers': [
+        {
+          'visibility': 'off',
+        },
+      ],
+    },
+    {
+      'featureType': 'water',
+      'elementType': 'labels.text',
+      'stylers': [
+        {
+          'visibility': 'off',
+        },
+      ],
+    },
+  ],
+};
+
+
