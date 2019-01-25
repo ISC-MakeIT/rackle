9b2c119e1a585204e9f9f92a625e9d08d4a3fdb4
yamato0331 <yamatotunasima1@gmail.com>
2019-01-25 12:00:31 +0900

    リストの追加

---
+++ src/screens/ElevatorScreen.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, Image, Dimensions } from 'react-native';
+
+export default class GuideScreen extends React.Component<{}, {}> {
+
+}
