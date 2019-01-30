2d02acd0ded6b1a25fb98d2f42d026970961caef
fumihumi <t.fum1hum@gmail.com>
2019-01-30 14:24:26 +0900

    :sparkles: ダミーデータ入れたり、ホーム画面の挙動変えたり

---
+++ src/dummydata/stations.ts
@@ -0,0 +1,13 @@
+import { Stations } from '../domains/station';
+
+export const StationData: Stations = {
+  'station': { 'id': 1, 'name': '横浜' },
+  'train_lines': [
+    { 'id': 1, 'name': 'JR' },
+    { 'id': 2, 'name': '東急東横' },
+    { 'id': 3, 'name': '京急' },
+    { 'id': 4, 'name': '相鉄' },
+    { 'id': 5, 'name': 'ブルーライン' },
+    { 'id': 6, 'name': 'みなとみらい' },
+  ],
+};
