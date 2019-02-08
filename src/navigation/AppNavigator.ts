c86eb12212228fe93be21b50fcd3af00e374d676
fumihumi <t.fum1hum@gmail.com>
2019-02-08 18:55:33 +0900

    :bug: 画面遷移ができてなかった

--- src/navigation/AppNavigator.ts
+++ src/navigation/AppNavigator.ts
@@ -1,8 +1,14 @@
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 
+import GuideScreen from '../screens/GuideScreen';
 import HomeScreen from '../screens/HomeScreen';
 
 export default createAppContainer(
-  createStackNavigator({ Home: { screen: HomeScreen, }, })
+  createStackNavigator(
+    {
+      Home: { screen: HomeScreen },
+      Guide: { screen: GuideScreen },
+    }
+  )
 );
 
