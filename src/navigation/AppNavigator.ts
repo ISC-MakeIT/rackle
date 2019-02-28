c06084a6522130a6e34073260a499b6fedcedb33
yamato0331 <yamatotunasima1@gmail.com>
2019-02-28 11:53:48 +0900

    elevatorscrren削除

--- src/navigation/AppNavigator.ts
+++ src/navigation/AppNavigator.ts
@@ -2,14 +2,12 @@ import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 import GuideScreen from '../screens/GuideScreen';
 import HomeScreen from '../screens/HomeScreen';
-import ElevatorScreen from '../screens/ElevatorScreen';
 
 export default createAppContainer(
   createStackNavigator(
     {
       Home: { screen: HomeScreen },
       Guide: { screen: GuideScreen },
-      Elevator: { screen: ElevatorScreen },
     }
   )
 );
